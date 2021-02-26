a5128cf2aae2a14ccdb97f7dc9532cce94fb279c
Gleryston Matos <glerystonmatos@yahoo.com.br>
2021-02-26 12:51:09 -0300

    Atualização das dependências do projeto.

--- Quiron.Repositorie/Repositories/EspacoRepository.cs
+++ Quiron.Repositorie/Repositories/EspacoRepository.cs
@@ -1,29 +1,14 @@
-﻿using AutoMapper;
-using AutoMapper.QueryableExtensions;
-using Quiron.Data.Comun;
+﻿using Quiron.Data.Comun;
 using Quiron.Data.Context;
-using Quiron.Domain.Dto;
 using Quiron.Domain.Entities;
 using Quiron.Domain.Interfaces.Data;
-using System;
-using System.Linq;
-using System.Linq.Expressions;
 
 namespace Quiron.Data.Repositories
 {
     public class EspacoRepository : Repository<Espaco>, IEspacoRepository
     {
-        private readonly IMapper _mapper;
-
-        public EspacoRepository(QuironContext context, IMapper mapper) : base(context)
+        public EspacoRepository(QuironContext context) : base(context)
         {
-            _mapper = mapper;
         }
-
-        public new EspacoDto FindById(Guid id)
-            => _context.Set<Espaco>().Where(e => e.Id.Equals(id)).ProjectTo<EspacoDto>(_mapper.ConfigurationProvider).FirstOrDefault();
-
-        public new EspacoDto FindBy(Expression<Func<Espaco, bool>> predicate)
-            => _context.Set<Espaco>().Where(predicate).ProjectTo<EspacoDto>(_mapper.ConfigurationProvider).FirstOrDefault();
     }
 }
